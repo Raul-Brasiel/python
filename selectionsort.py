c2a929f6a1b49836d29aed1f3c43369eb27d5684
Raul <82548841+Raul-Brasiel@users.noreply.github.com>
2025-08-15 15:47:07 -0300

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,34 @@
+import random
+def organizar_lista(vetor,qtd):
+  valor_menor = max(vetor)+1
+  print('Organizando o vetor...')
+  for i in range(0,qtd):
+    trocou = False
+    for j in range(i+1,qtd):
+      if vetor[j] < vetor[i]:
+        if vetor[j] < valor_menor:
+          index_menor = j
+          valor_menor = vetor[j]
+          trocou = True
+    if trocou:
+      vetor.insert(i,vetor[index_menor])
+      vetor.pop(index_menor+1)
+      valor_menor = max(vetor)+1
+    print(f'{vetor}')
+  return vetor
+
+qtd = int(input('Digite o tamanho da lista de números: '))
+opcao = int(input('1-Digitar números\n2-Preencher automático\n'))
+vetor = []
+
+if opcao == 1:
+  for i in range(0, qtd):
+    vetor.append(int(input(f'Digite um número para a posição {i}: ')))
+elif opcao == 2:
+  vetor = [random.randint(1, 50) for _ in range(qtd)]
+else:
+  print('Opcão inválida!')
+
+print(f'Vetor: {vetor}')
+
+print(f'Vetor: {organizar_lista(vetor,qtd)}')
